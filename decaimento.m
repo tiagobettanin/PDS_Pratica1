daa1860fa2d6693d8e2003cc6bcbab58b1b40ef4
Tiago Bettanin <92163041+tiagobettanin@users.noreply.github.com>
2025-10-05 23:21:17 -0300

    Update decaimento.m

--- decaimento.m
+++ decaimento.m
@@ -1,22 +1,5 @@
 function y = decaimento(x, fs, alpha)
-    % decaimento - Aplica um envelope de decaimento exponencial a um sinal.
-    %
-    % Sintaxe:
-    %   y = decaimento(x, fs, alpha)
-    %
-    % Entradas:
-    %   x     - Vetor de áudio de entrada.
-    %   fs    - Frequência de amostragem (Hz).
-    %   alpha - Coeficiente de decaimento. Valores maiores resultam em
-    %           decaimento mais rápido. Um valor de 5 é um bom começo.
-    %
-    % Saída:
-    %   y     - Vetor de áudio com o envelope de decaimento.
-    
-    % Número de amostras
     N = length(x);
-    
-    % Vetor de tempo
     t = (0:N-1) / fs;
     
     % Envelope de decaimento exponencial
@@ -24,4 +7,5 @@ function y = decaimento(x, fs, alpha)
     
     % Aplica o envelope ao sinal de áudio
     y = x(:)' .* envelope;
+
 end
