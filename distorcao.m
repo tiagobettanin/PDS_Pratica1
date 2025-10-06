2d296172c2adb0c8587b9e4b822ac2a03f3c438b
Tiago Bettanin <92163041+tiagobettanin@users.noreply.github.com>
2025-10-05 23:22:06 -0300

    Update distorcao.m

--- distorcao.m
+++ distorcao.m
@@ -1,18 +1,4 @@
 function y = distorcao(x, L)
-    % distorcao - Aplica distorção por clipping simétrico.
-    %
-    % Sintaxe:
-    %   y = distorcao(x, L)
-    %
-    % Entradas:
-    %   x - Vetor de áudio de entrada.
-    %   L - Limiar (threshold) de clipping (0 < L <= 1). Valores menores
-    %       causam mais distorção.
-    %
-    % Saída:
-    %   y - Vetor de áudio distorcido.
-
-    % Garante que L seja positivo
     if L <= 0
         error('O limiar L deve ser um valor positivo.');
     end
@@ -21,4 +7,5 @@ function y = distorcao(x, L)
     % 1. Encontra todos os valores acima de L e os define como L.
     % 2. Encontra todos os valores abaixo de -L e os define como -L.
     y = max(min(x, L), -L);
+
 end
