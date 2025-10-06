91e4a1be30a3cbffff9936945a1626cc0c154d6b
Tiago Bettanin <92163041+tiagobettanin@users.noreply.github.com>
2025-10-05 23:30:04 -0300

    Update insereSample.m

--- insereSample.m
+++ insereSample.m
@@ -1,19 +1,4 @@
 function musica = insereSample(musica, sample, t0, fs)
-    % insereSample - Insere um sample de áudio dentro de um vetor música
-    %
-    % Sintaxe:
-    %   musica = insereSample(musica, sample, t0, fs)
-    %
-    % Entradas:
-    %   musica - vetor contendo a música (áudio principal)
-    %   sample - vetor contendo o áudio a ser inserido
-    %   t0     - instante de inserção em segundos
-    %   fs     - frequência de amostragem (Hz)
-    %
-    % Saída:
-    %   musica - vetor de áudio com o sample inserido
-    
-    % Calcula o índice de início
     inicio = round(t0 * fs) + 1;
     fim = inicio + length(sample) - 1;
     
@@ -22,6 +7,5 @@ function musica = insereSample(musica, sample, t0, fs)
         musica(end+1:fim) = 0; % completa com zeros
     end
     
-    % Insere o sample somando no intervalo correspondente
     musica(inicio:fim) = musica(inicio:fim) + sample(:)'; 
 end
