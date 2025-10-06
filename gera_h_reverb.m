50e5bd6b6b81c5ee27845793cdc9613cfcb55a1f
Tiago Bettanin <92163041+tiagobettanin@users.noreply.github.com>
2025-10-05 23:29:32 -0300

    Update gera_h_reverb.m

--- gera_h_reverb.m
+++ gera_h_reverb.m
@@ -1,16 +1,4 @@
 function h = gera_h_reverb(alpha, N, fs)
-    % gera_h_reverb - Cria uma RI de reverb curto com ruído.
-    %
-    % Sintaxe: h = gera_h_reverb(alpha, N, fs)
-    %
-    % Entradas:
-    %   alpha - Coeficiente de decaimento. Controla a "cauda" do reverb.
-    %   N     - Duração da RI em número de amostras.
-    %   fs    - Frequência de amostragem (Hz).
-    %
-    % Saída:
-    %   h     - Vetor da resposta ao impulso.
-
     % Vetor de tempo discreto (n) e contínuo (t)
     n = 0:N-1;
     t = n / fs;
@@ -21,6 +9,7 @@ function h = gera_h_reverb(alpha, N, fs)
     % Gera a RI conforme a fórmula h[n] = exp(-alpha*t) * w[n]
     h = exp(-alpha * t) .* w;
     
-    % Normaliza a RI para um pico de 1
+    % Normaliza para um pico de 1
     h = h / max(abs(h));
+
 end
